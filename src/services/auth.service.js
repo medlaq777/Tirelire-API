3fb3ce97be7b72d67aa069cd445b78e2c603dc34
medlaq777 <m.laqsoumi@gmail.com>
2025-10-08 11:16:16 +0100

    Update: the auth.service.js file with make it as classe and also handle the functionalities necessary

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,21 +1,65 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user.model");
-const jwToken = require("../utils/jwt");
+const bycrypt = require("bcryptjs");
+const userRepo = require("../repositories/user.repository");
+const jwt = require("../utils/jwt");
+const config = require("../config/config");
 
-exports.register = async (userData) => {
-  const { email, password } = userData;
-  const UserExists = await User.findOne({ email });
-  if (UserExists) throw new Error("Email already in use");
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ email, hashedPassword });
-  const token = jwToken.generateToken(newUser);
-  return { user: newUser, token };
-};
-exports.login = async (userData) => {
-  const user = await User.findOne({ email });
-  if (!user) throw new Error("Invalid email or password");
-  const isSame = await bcrypt.compare(password, user.password);
-  if (!isSame) throw new Error("Invalid email or password");
-  const token = jwToken.generateToken(user);
-  return { user, token };
-};
+class AuthService {
+  constructor(userRepo) {
+    this.userRepo = userRepo;
+  }
+  async register({ email, password }) {
+    if (!email || !password) {
+      const err = new Error("Email and Password are required");
+      err.status = 400;
+      throw err;
+    }
+    const userExists = await this.userRepo.findByEmail(email);
+    if (userExists) {
+      const err = new Error("User already exists");
+      err.status = 409;
+      throw err;
+    }
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await this.userRepo.create({ email, password: hashed });
+    const token = jwt.generateToken({ id: user.id, email: user.email });
+    return { user: this.sanitize(user), token };
+  }
+
+  async login({ email, password }) {
+    if (!email || !password) {
+      const err = new Error("Email and Password are required");
+      err.status = 400;
+      throw err;
+    }
+    const user = await this.userRepo.findByEmail(email);
+    if (!user) {
+      const err = new Error("Invalid email or password");
+      err.status = 401;
+      throw err;
+    }
+    const isSame = await bcrypt.compare(password, user.password);
+    if (!isSame) {
+      const err = new Error("Invalid email or password");
+      err.status = 401;
+      throw err;
+    }
+    const token = jwt.generateToken({ id: user.id, email: user.email });
+    return { user: this.sanitize(user), token };
+  }
+
+  async profile(userId) {
+    const user = await this.userRepo.findById(userId);
+    if (!user) {
+      const err = new Error("User not found");
+      err.status = 404;
+      throw err;
+    }
+    return this.sanitize(user);
+  }
+  sanitize(user) {
+    const { password, __v, ...rest } =
+      user && user.toObject ? user.toObject() : user;
+    return rest;
+  }
+}
+module.exports = new AuthService(userRepo);
