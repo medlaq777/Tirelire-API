4f642be661b1db7e14d6573ff35b7216907604be
medlaq777 <m.laqsoumi@gmail.com>
2025-10-08 21:06:34 +0100

    Update: The user.models with add the Full Name and the role

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    fullName: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, minlength: 6, trim: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   { timestamps: true }
 );
