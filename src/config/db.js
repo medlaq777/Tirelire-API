80edd4a68a93f186a0af35f7d98533f317507211
medlaq777 <m.laqsoumi@gmail.com>
2025-10-08 10:54:28 +0100

    Update: the db.js file with use classes and make the connection singleton

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
 const config = require("./config");
 
-let instance = null;
+class database {
+  constructor() {
+    this.connect = false;
+  }
+  async connectDB() {
+    if (this.connect) return;
+    if (!config.mongoUri) throw new Error("MongoDB URI not provided in config");
+    try {
+      await mongoose.connect(config.mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      this.connect = true;
+      console.log("OK");
+    } catch (err) {
+      console.error("mongoDB connection error:", err);
+      throw err;
+    }
+  }
+}
 
-const connectDB = async () => {
-  if (instance) return instance;
-  instance = await mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log("OK");
-  return instance;
-};
-module.exports.connectDB();
+module.exports = new database();
