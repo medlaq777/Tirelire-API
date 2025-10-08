b69a959ebfc81d368a26da63f89a49abbbada5e3
medlaq777 <m.laqsoumi@gmail.com>
2025-10-08 11:28:04 +0100

    Update: auth.middleware.js file with handle static function to protect routes needed login before

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,18 +1,20 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config/config");
-
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeder) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, config.jwtSecret, (err, decoded) =>
-  {
-    if (err) {
-      return res.status(403).json({ message: "Invalid or expired token" });
+class AuthMiddleware {
+  static protect(req, res, next) {
+    try {
+      const header = req.headers.authorization;
+      if (!header || !header.startsWith("Bearer ")) {
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        throw err;
+      }
+      const token = header.split(" ")[1];
+      const payload = jwt.verify(token);
+      req.user = payload;
+      next();
+    } catch (err) {
+      next(err);
     }
-    req.user = decoded;
-    next();
-  });
-};
+  }
+}
+module.exports = AuthMiddleware;
