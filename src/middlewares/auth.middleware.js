2220bb2d4aad64640ed89d59dccb2404ac8adef7
medlaq777 <m.laqsoumi@gmail.com>
2025-10-10 22:55:51 +0100

    Update: the auth middleware with add authorizeRole function

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -17,6 +17,26 @@ class AuthMiddleware {
       next(err);
     }
   }
+
+  static authorizeRole(role) {
+    return (req, res, next) => {
+      try {
+        if (!req.user) {
+          const e = new Error("Unauthorized");
+          e.status = 401;
+          throw e;
+        }
+        if (req.user.role !== role) {
+          const e = new Error("Forbidden");
+          e.status = 403;
+          throw e;
+        }
+        next();
+      } catch (err) {
+        next(err);
+      }
+    };
+  }
 }
 
 export default AuthMiddleware;
