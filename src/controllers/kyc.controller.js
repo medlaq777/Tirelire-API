ffeda00e35f44ea4b9a4a90eb4746c16e0f2f304
medlaq777 <m.laqsoumi@gmail.com>
2025-10-10 22:41:46 +0100

    Create: Kyc Controller to controller and upload route for kyc

---
+++ src/controllers/kyc.controller.js
@@ -0,0 +1,48 @@
+import Kyc from "../services/kyc.service";
+
+class KycController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  async submit(req, res, next) {
+    try {
+      const { fullname, nationalId } = req.body;
+      const userId = req.user.id;
+      if (!req.files || !req.files["idImage"] || !req.files["selfie"]) {
+        const e = new Error("required ID image and Selfie");
+      }
+      const idBuf = req.files["idImage"][0].buffer;
+      const selfieBuf = req.files["selfie"][0].buffer;
+      const result = await this.service.submitKyc({
+        userId,
+        fullname,
+        nationalId,
+        idImgBuffer: idBuf,
+        selfieBuffer: selfieBuf,
+      });
+      res.status(201).json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async adminApprove(req, res, next) {
+    try {
+      const { kycId } = req.params;
+      const { approve, note } = req.body;
+      const adminId = req.user.id;
+      const updated = await this.service.adminApprove(
+        kycId,
+        approve === true || approve === "true",
+        adminId,
+        note
+      );
+      res.json({ updated });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = new KycController(Kyc);
