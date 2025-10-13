c7ef2a7bc1c00f275de1d2151ee970576b4a0664
medlaq777 <m.laqsoumi@gmail.com>
2025-10-13 09:25:28 +0100

    Create: the Groupe Model with scema of all data necessary

---
+++ src/models/group.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  amount: { type: Number, required: true },
+  deadline: { type: Date, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+  status: { type: String, enum: ["active", "closed"], default: "active" },
+});
+
+export default mongoose.model("Group", userSchema);
