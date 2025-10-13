5d59ece4628f37c90b938d1e0830195405e249d6
medlaq777 <m.laqsoumi@gmail.com>
2025-10-13 09:31:38 +0100

    Create: The GroupRepository file with all function needs like create finds and update group

---
+++ src/repositories/group.repository.js
@@ -0,0 +1,28 @@
+import Group from "../models/group.model.js";
+
+class GroupRepository {
+  async create(data) {
+    return new Group(data).save();
+  }
+
+  async findById(id) {
+    return Group.findById(id).populate("owner members").exec();
+  }
+  async findByOwner(ownerId) {
+    return Group.find({ owner: ownerId }).exec();
+  }
+
+  async joinGroup(groupId, userId) {
+    return Group.findByIdAndUpdate(
+      groupId,
+      { $addToSet: { members: userId } },
+      { new: true }
+    ).exec();
+  }
+
+  async update(groupeId, data) {
+    return Group.findByIdAndUpdate(groupId, data, { new: true });
+  }
+}
+
+export default new GroupRepository();
