3feb46a1670038d7e21a58c2a6dac0e3840c3a1c
medlaq777 <m.laqsoumi@gmail.com>
2025-10-14 14:16:08 +0100

    Finish instalation for sonarqube

--- src/test/auth.service.test.js
+++ src/test/auth.service.test.js
@@ -1,11 +1,4 @@
-import {
-  jest,
-  describe,
-  it,
-  expect,
-  beforeEach,
-  beforeAll,
-} from "@jest/globals";
+import { jest, describe, it, expect, beforeEach } from "@jest/globals";
 
 import bcrypt from "bcryptjs";
 import jwt from "../utils/jwt.js";
