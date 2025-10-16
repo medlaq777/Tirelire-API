3345204bb730bed2bb7dbb430ec5bf5356603ba2
medlaq777 <m.laqsoumi@gmail.com>
2025-10-16 18:44:48 +0100

    Create: Tickets model and create the schema db

---
+++ src/models/ticket.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema(
+  {
+    group: { type: mongoose.Schema.ObjectId, ref: "Group", required: true },
+    user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+    subjects: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["open", "resolved", "closed"],
+      default: "open",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Ticket", ticketSchema);
