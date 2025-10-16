359d244260115f9219ffdf6465cb456f1228a849
medlaq777 <m.laqsoumi@gmail.com>
2025-10-16 14:45:33 +0100

    Create: the Payment Service and handle functionality of payment

---
+++ src/services/payment.service.js
@@ -0,0 +1,53 @@
+import Stripe from "stripe";
+import Config from "../config/config.js";
+import PaymentRepository from "../repositories/payment.repository.js";
+import GroupRepository from "../repositories/group.repository.js";
+
+class PaymentServie {
+  constructor(paymentRepo, groupRepo) {
+    this.stripe = new Stripe(Config.stripeSecret);
+    this.paymentRepo = paymentRepo;
+    this.groupRepo = groupRepo;
+  }
+
+  async createPaymentIntent({ groupId, userId, amount }) {
+    const group = await this.groupRepo.findById(groupId);
+    if (!group) {
+      const e = new Error("Group not found");
+      e.status = 404;
+      throw e;
+    }
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: Math.round(amount * 100),
+      currency: "usd",
+      metadata: { groupId, userId },
+    });
+
+    const payment = await this.paymentRepo.create({
+      group: groupId,
+      member: userId,
+      amount,
+      stripePaymentId: paymentIntent.id,
+    });
+    return { clientSecret: paymentIntent.client_secret, payment };
+  }
+
+  async handleWebhook(event) {
+    const { type, data } = event;
+    if (type === "payment_intent.succeeded") {
+      const paymentIntent = data.object;
+      const payment = await this.paymentRepo.findByStripeId(paymentIntent.id);
+      if (payment)
+        await this.paymentRepo.updateStatus(payment._id, "succeeded");
+    }
+
+    if (type === "payment_intent.payment_failed") {
+      const paymentIntent = data.object;
+      const payment = await this.paymentRepo.findByStripeId(paymentIntent.id);
+      if (payment) await this.paymentRepo.updateStatus(payment._id, "failed");
+    }
+  }
+}
+
+export default PaymentServie(PaymentRepository, GroupRepository);
