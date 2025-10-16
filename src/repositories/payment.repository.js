73c254d7bdc53453b72c3063fa69e45ae3d52385
medlaq777 <m.laqsoumi@gmail.com>
2025-10-16 14:08:00 +0100

    Create: the Payment repository with handling the functionalities missed

---
+++ src/repositories/payment.repository.js
@@ -0,0 +1,17 @@
+import Payment from "../models/payment.model.js";
+
+class PaymentRepository {
+  async create(data) {
+    return new Payment(data).save();
+  }
+
+  async updateStatus(id, status) {
+    return Payment.findByIdAndUpdate(id, { status }, { new: true });
+  }
+
+  async findByStripId(stripeId) {
+    return Payment.findOne({ stripePaymentId: stripeId }).exec();
+  }
+}
+
+export default new PaymentRepository();
