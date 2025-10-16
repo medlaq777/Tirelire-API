5d9a2d55660181e59f35537acf335b70d2b169de
medlaq777 <m.laqsoumi@gmail.com>
2025-10-16 19:46:31 +0100

    Create: TicketRoutes with all endpoints necessary

---
+++ src/routes/ticket.routes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
+import TicketController from "../controllers/ticket.controller.js";
+
+class TicketRoutes {
+  static build() {
+    const router = express.Router();
+
+    router.post(
+      "/tickets",
+      AuthMiddleware.protect,
+      TicketController.create.bind(TicketController)
+    );
+    router.get(
+      "/tickets",
+      AuthMiddleware.protect,
+      TicketController.listUser.bind(TicketController)
+    );
+
+    router.get(
+      "/admin/all",
+      AuthMiddleware.protect,
+      AuthMiddleware.authorizeRole("admin"),
+      TicketController.listAll.bind(TicketController)
+    );
+
+    router.put(
+      "/admin/:ticketId/status",
+      AuthMiddleware.protect,
+      AuthMiddleware.authorizeRole("admin"),
+      TicketController.resolve.bind(TicketController)
+    );
+
+    return router;
+  }
+}
+
+export default TicketRoutes;
