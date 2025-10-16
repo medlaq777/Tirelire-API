b7a0775618a791a53c10ea795921c09b2e65379b
medlaq777 <m.laqsoumi@gmail.com>
2025-10-16 19:35:51 +0100

    Create: ticketController with all functionalities necessary

---
+++ src/controllers/ticket.controller.js
@@ -0,0 +1,45 @@
+import TicketService from "../services/ticket.service.js";
+
+class TicketController {
+  async create(req, res, next) {
+    try {
+      const user = req.user.id;
+      const { group, subject, message } = req.body;
+      const ticket = await TicketService.createTicket({
+        user,
+        group,
+        subject,
+        message,
+      });
+      res.status(201).json(ticket);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async listAll(req, res, next) {
+    try {
+      const ticket = await TicketService.listAllTickets();
+      res.json(ticket);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async resolve(req, res, next) {
+    try {
+      const { ticketId } = req.parms;
+      const { status } = req.body;
+      const result = await TicketService.resolveTicket(
+        ticketId,
+        req.user.id,
+        status
+      );
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default new TicketController();
