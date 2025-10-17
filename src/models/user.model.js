0b4101fc9afd95cc1c2486f62449d4f400fcee54
medlaq777 <m.laqsoumi@gmail.com>
2025-10-17 11:36:55 +0100

    Update: the User model with add the reliability score key

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, minlength: 6, trim: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
+    isKycVerified: { type: Boolean, default: false },
+    reliabilityScore: { type: Number, default: 100 },
   },
   { timestamps: true }
 );
